d117e42ca85f042655f8ef526a3f8cfd568aebc1
Wil Thieme <wil.thieme@protonmail.com>
2024-02-28 21:08:26 -0500

    pass/fail enum class

--- tests/feedcheck_checks.rb
+++ tests/feedcheck_checks.rb
@@ -4,13 +4,18 @@ require 'faraday'
 require 'nokogiri'
 require 'uri'
 
+class Status
+  FAILED = true
+  PASSED = false
+end
+
 def check_avatar(avatar, av_dir, faraday)
-  return ['_ ', false] unless avatar
+  return ['_ ', Status::PASSED] unless avatar
 
   return check_url(avatar, faraday) if avatar.include? '//'
-  return ["✗\nAvatar not found: hackergotchi/#{avatar} ", true] unless File.file?("#{av_dir}/#{avatar}")
+  return ["✗ Avatar not found: #{av_dir}/#{avatar} ", Status::FAILED] unless File.file?("#{av_dir}/#{avatar}")
 
-  ['✓ ', false]
+  ['✓ ', Status::PASSED]
 end
 
 def check_url(url, faraday)
@@ -19,21 +24,21 @@ def check_url(url, faraday)
   begin
     res = faraday.get(URI(url))
   rescue Faraday::ConnectionFailed
-    return ["#{error_message}Connection Failure when trying to access '#{url}' ", true]
+    return ["#{error_message}Connection Failure when trying to access '#{url}' ", Status::FAILED]
   rescue Faraday::TimeoutError
-    return ["#{error_message}Connection Timeout waiting for '#{url}' ", true]
+    return ["#{error_message}Connection Timeout waiting for '#{url}' ", Status::FAILED]
   rescue Faraday::SSLError
-    return ["#{error_message}SSL Error when trying to access '#{url}' ", true]
+    return ["#{error_message}SSL Error when trying to access '#{url}' ", Status::FAILED]
   end
 
   error = "#{error_message}Non successful status code #{res.status} when trying to access '#{url}' "
   if res.status.to_i.between?(300, 399) && res.headers.key?('location')
-    return ["#{error}. Try using '#{res.headers['location']}' instead", true]
+    return ["#{error}. Try using '#{res.headers['location']}' instead", Status::FAILED]
   end
 
-  return [error, true] unless res.status.to_i == 200
+  return [error, Status::FAILED] unless res.status.to_i == 200
 
-  ['✓ ', false]
+  ['✓ ', Status::PASSED]
 end
 
 def check_urls(url_arr, faraday)
@@ -43,29 +48,29 @@ def check_urls(url_arr, faraday)
 end
 
 def parse_xml(feed, faraday)
-  result = ['✗ ', true]
+  result = ['✗ ', Status::FAILED]
 
   begin
     xml = faraday.get(URI(feed))
   rescue Faraday::ConnectionFailed
-    return ["#{result.first}Connection Failure when trying to read XML from '#{feed}' ", true]
+    return ["#{result.first}Connection Failure when trying to read XML from '#{feed}' ", Status::FAILED]
   rescue Faraday::SSLError
-    return ["#{result.first}SSL Error when trying to read XML from '#{feed}' ", true]
+    return ["#{result.first}SSL Error when trying to read XML from '#{feed}' ", Status::FAILED]
   end
 
   xml_err = Nokogiri::XML(xml.body).errors
-  return ["#{result.first}Unusable XML syntax: #{feed}\n#{xml_err} ", true] unless xml_err.empty?
+  return ["#{result.first}Unusable XML syntax: #{feed}\n#{xml_err} ", Status::FAILED] unless xml_err.empty?
 
-  ['✓ ', false]
+  ['✓ ', Status::PASSED]
 end
 
 def check_unused_files(av_dir, avatars)
   hackergotchis = Dir.foreach(av_dir).select { |f| File.file?("#{av_dir}/#{f}") }
   diff = (hackergotchis - avatars)
 
-  return [nil, false] if diff.empty?
+  return ["There are unused files in #{av_dir}: #{diff.sort.join(', ')}", Status::FAILED] unless diff.empty?
 
-  ["There are unused files in hackergotchis: #{diff.sort.join(', ')}", true]
+  [nil, Status::PASSED]
 end
 
 def accumulate_results(result, did_fail, new_result)
@@ -79,12 +84,12 @@ def check_source(key, section, faraday)
   avatar, link, feed = %w[avatar link feed].map { |k| section[k] if section.key?(k) }
 
   avatar_result = check_avatar(avatar, AV_DIR, faraday)
-  did_fail = accumulate_results(result, false, avatar_result)
+  did_fail = accumulate_results(result, Status::PASSED, avatar_result)
 
   url_result = check_urls([link, feed], faraday)
   did_fail = accumulate_results(result, did_fail, url_result)
 
-  xml_result = url_result.last ? ['_ ', false] : parse_xml(feed, faraday)
+  xml_result = url_result.last ? ['_ ', Status::PASSED] : parse_xml(feed, faraday)
   did_fail = accumulate_results(result, did_fail, xml_result)
 
   [[result.compact.join, did_fail], avatar]
