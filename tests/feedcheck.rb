931c5c7531bb8b1fae36eef418ab2c5328c1257b
Ramona T <ramona.t.me@pm.me>
2026-08-28 21:44:01 -0400

    feed-check updates (#232)
    * test
    
    * Delete tests/feedcheck_job_summary.rb
    
    * Enhance concurrency and error reporting in feedcheck
    
    Refactor feedcheck script to improve concurrency and error handling.
    
    * Refactor feedcheck_checks.rb
    
    * Refactor error message handling in feedcheck.rb
    
    Updated error message formatting and summary file structure.
    
    * Update error message formatting in feedcheck_checks
    
    * Improve error handling and warning messages in feedcheck
    
    Refactor error handling in feed processing loop and update warning message format.
    
    * Refactor check_status_and_location method
    
    Refactor check_status_and_location to remove url parameter and handle URI parsing with error rescue.
    
    * Fix syntax from 'elseif' to 'elsif' in Ruby
    
    * Handle Google bot challenge in feedcheck checks
    
    * Improve error message formatting in feedcheck.rb
    
    Updated error message formatting for better clarity in output and summary files.
    
    * Update error_messages handling in SourceResult
    
    Changed error_messages to not join with newline.
    
    * Fix error message formatting in feedcheck.rb
    
    * Refactor error message handling in feedcheck.rb
    
    * Fix error message formatting and update avatar file type
    
    * Refactor constants for better readability

--- tests/feedcheck.rb
+++ tests/feedcheck.rb
@@ -4,56 +4,84 @@ require 'faraday'
 require 'inifile'
 
 require_relative 'feedcheck_checks'
-require_relative 'feedcheck_job_summary'
 
-INI_FILE = 'planet.ini'
-AV_DIR = 'hackergotchi'
-
-def write_to_file(contents, filename)
-  File.open(filename, 'w') { |file| file.write contents.join }
-end
-
-planet_srcs = IniFile.load(INI_FILE).to_h
-did_any_fail = false
-error_messages = []
-avatars = ['default.webp']
+INI_FILE = 'planet.ini'           # ini file containing library of feeds
+DEFAULT_AVATAR = 'default.webp'   # name of image to use if avatar is not provided
+AV_DIR = 'hackergotchi'           # folder containing local feed avatars
+WORKER_COUNT = 3                  # number of concurrent workers
+FEED_NAME_PADDING = 48            # number of characters before each ``=>`` in log output
 
 faraday = Faraday.new(request: { open_timeout: 10 }) do |f|
   f.adapter :net_http
 end
 
 queue = Queue.new
-planet_srcs.each do |key, section|
-  queue.push([key, section]) if ARGV.empty? || ARGV.include?(key)
+known_feed_names = []
+IniFile.load(INI_FILE).to_h.each do |feed_name, section|
+  known_feed_names << feed_name
+  queue.push([feed_name, section]) if ARGV.empty? || ARGV.include?(feed_name)
 end
 
-workers = (0...4).map do
-  Thread.new do
-    until queue.empty?
-      key, section = queue.pop
-      next unless section.is_a?(Hash) && (key != 'global')
+error_messages = []
+did_any_fail = false
 
-      res, avatar = check_source(key, section, faraday)
-      avatars << avatar
-      puts res.first
-      error_messages << res.first if res.last
-      did_any_fail ||= res.last
+missing_feed_names = ARGV - known_feed_names
+missing_feed_names.each do |feed_name|
+  puts "#{feed_name.ljust(FEED_NAME_PADDING)} =>  not found in #{INI_FILE}"
+  error_messages << "#{feed_name}\nFeed not found in #{INI_FILE}"
+  did_any_fail = true
+end
+
+puts "#{'::notice::Feed Errors Summary'.ljust(FEED_NAME_PADDING)} =>  (avatar) (link) (feed) (xml)"
+
+avatars = [DEFAULT_AVATAR]
+mutex = Mutex.new
+
+workers = Array.new(WORKER_COUNT) do
+  Thread.new do
+    loop do
+      begin
+        feed_name, section = queue.pop(true)
+      rescue
+        break
+      end
+      next unless section.is_a?(Hash) && feed_name != 'global'
+
+      result = check_source(feed_name, section, faraday, AV_DIR)
+      puts "#{feed_name.ljust(FEED_NAME_PADDING)} =>  #{result.symbols}"
+
+      mutex.synchronize do
+        avatars << result.avatar
+        error_messages << result.error_messages.unshift(feed_name) if result.failed
+        did_any_fail ||= result.failed
+      end
     end
   end
 end
 workers.each(&:join)
 
-unused_files_result = check_unused_files(AV_DIR, avatars) if ARGV.empty? || ARGV[0].nil? || !ARGV[0]
-puts "::warning::#{unused_files_result.first}" unless unused_files_result.nil? || !unused_files_result.last
+run_unused_check = ARGV.empty? || ARGV[0].nil?
+unused_files_message = run_unused_check ? check_unused_files(AV_DIR, avatars) : nil
+
 
 if did_any_fail
-  puts 'Feed Errors Summary => (avatar) (link) (feed) (xml)'
-  error_messages.each do |message|
-    puts "::error#{message}"
+  error_messages.each { |message| puts "::group::#{message.join("\n::error::#{message.first}: ")}\n::endgroup::" }
+
+  File.open('error-summary.md', 'w') do |file|
+    file.write "# Summary\n"
+    file.write "\n## Error Summary\n"
+    error_messages.each { |message| file.write "\n### #{message.join("\n")}\n" }
+    if unused_files_message
+      puts "::warning::#{unused_files_message}"
+      file.write "\n## Warning Summary\n"
+      file.write "\n#{unused_files_message}\n"
+    end
   end
-  error_messages << unused_files_result.first unless unused_files_result.nil? || !unused_files_result.last
-  create_job_summary(error_messages)
+
   abort
+elsif unused_files_message
+  puts "::warning::#{unused_files_message}"
 end
+
 File.delete('error-summary.md') if File.exist?('error-summary.md')
 puts '::notice::All feeds passed checks!'
